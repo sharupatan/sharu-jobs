47799c95b7ab6d84352b223b983077879ed2a4c0
sharukhan <sharukhan@webkorps.com>
2024-03-27 11:40:35 -0500

    implememted rough validation and login and logout

--- app/javascript/components/LoginPage.js
+++ app/javascript/components/LoginPage.js
@@ -1,17 +1,58 @@
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { Container } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
+const setType = (status) => {
+  return status ? 'text' : 'password'
+}
+
+const checkValidity = (email, pass) => {
+  console.log(email.endsWith('@gmail.com') && (pass.length > 6))
+  const cond = email.endsWith('@gmail.com') && (pass.length >= 6)
+  return cond ? 'success' : 'danger'
+}
+
 const LoginPage = () => {
-  console.log('came to login')
-  localStorage.setItem('cred',JSON.stringify('asdfghjkl'))
+  const [loginEmail,setLoginEmail] = useState('')
+  const [loginPass,setLoginPass] = useState('')
+  const [loginPassHideStatus, setLoginPassHideStatus] = useState(false)
+  const passInputType = useMemo(()=>setType(loginPassHideStatus),[loginPassHideStatus])
+  const isDetailsValid = useMemo(()=>checkValidity(loginEmail,loginPass),[loginEmail,loginPass])
+
+  useEffect(()=>{
+    const loginStatus = localStorage.getItem('cred')
+    if(loginStatus !== null){
+      window.location.replace('/')
+    }
+  },[])
+
+  const onChangeEmail = (e) => {
+    setLoginEmail(e.target.value)
+  }
+
+  const onChangePassword = (e) => {
+    setLoginPass(e.target.value)
+  }
+
+  const onChangePassHideStatus = () => {
+    setLoginPassHideStatus(!loginPassHideStatus)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(loginEmail,loginPass)
+    // localStorage.setItem('cred',JSON.stringify('login key is so and so'))
+  }
+
+
+
   return (
     <Container>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control type="email" placeholder="Enter email" onChange={onChangeEmail} value={loginEmail}/>
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
@@ -19,12 +60,12 @@ const LoginPage = () => {
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type={passInputType} placeholder="Password" onChange={onChangePassword} value={loginPass}/>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
+          <Form.Check type="checkbox" label="Check me out" onChange={onChangePassHideStatus} checked={loginPassHideStatus}/>
         </Form.Group>
-        <Button variant="primary" type="submit">
+        <Button type="submit" variant={isDetailsValid}>
           Submit
         </Button>
       </Form>
--- app/javascript/components/Navbar.js
+++ app/javascript/components/Navbar.js
@@ -1,28 +1,40 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
 
-const Navbars = () => (
-  <Navbar className="bg-dark navbar-dark">
-    <Container>
-      <Link to="/" className="btn btn-primary me-2">
-        Sharu Jobs
-      </Link>
-      <Link to="/login" className="btn btn-secondary">
-        Login
-      </Link>
-      <Navbar.Toggle />
-      <Navbar.Collapse className="justify-content-end">
-        <Navbar.Text>
-          Signed in as:{" "}
-          <Link to="/" className="btn badge badge-info">
-            Sharu khan
+const Navbars = () => {
+  const loginStatus = localStorage.getItem("cred");
+  const handleLogout = () => {
+    localStorage.removeItem('cred')
+    window.location.replace('/login')
+  }
+  return (
+    <Navbar className="bg-dark navbar-dark">
+      <Container>
+        <Link to="/" className="btn btn-primary me-2">
+          Sharu Jobs
+        </Link>
+        {loginStatus === null ? (
+          <Link to="/login" className="btn btn-secondary">
+            Login
           </Link>
-        </Navbar.Text>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
-);
+        ) : (
+          <Button variant="danger" onClick={handleLogout}>Logout</Button>
+        )}
 
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            Signed in as:{" "}
+            <Link to="/" className="btn badge badge-info">
+              Sharu khan
+            </Link>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 export default Navbars;
