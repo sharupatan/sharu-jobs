90330aa005e9bf10f9fe6f4b1e61e251cd76ee91
sharukhan <sharukhan@webkorps.com>
2024-04-02 14:19:52 -0500

    structures files for redux

--- app/javascript/redux/store.js
+++ app/javascript/redux/store.js
@@ -1,5 +1,5 @@
 import {configureStore} from "@reduxjs/toolkit"
-import appReducer from "../redux/appSlice"
+import appReducer from "./slices/appSlice"
 
 export const store = configureStore({
     reducer: {
